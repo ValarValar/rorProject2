4d57dd69f11f6ac8609c66525bf5315bd45498a2
ValarValar <dan4eg98@mail.ru>
2020-11-30 22:53:43 +0300

    last commit session+

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,8 +36,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def index
-  		@articles = Article.all
-  		@article = Article.new
+  		@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
 
 	def destroy
